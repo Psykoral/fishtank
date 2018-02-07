df2b61134ca27098c013764b684138dd7b1bb4ce
jisutton <jisutton@adobe.com>
2018-02-07 00:20:10 -0800

    Updating format moving playlist to creds.js Adding multiLine options Expanding !help messages Adding connect / reconnect commands

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ var fs = require('fs');
 var logger = require('jethro');
 
 const coinPerPlay = 1;
-const list = 2926288; // Bot playlist ID. Login as them, then go to https://plug.dj/_/playlists to see the ID's.
 
 var bot = new PlugAPI({
 	email: creds.email,
 	password: creds.password
 });
 
-function fileChecks(){
+bot.multiLine = true; // Set to true to enable multi line chat. Default is false
+bot.multiLineLimit = 15; // Set to the maximum number of lines the bot should split messages up into. Any text beyond this number will just be omitted. Default is 5.
+
+function fileChecks() {
 	var fileUnparsed = '',
 		fileContents;
 
@@ -20,13 +22,13 @@ function fileChecks(){
 		fileContents = JSON.parse(fs.readFileSync('usr.json').toString());
 	} catch (e) {
 		logger('info', 'fileChecks Error', fileUnparsed + ':' + e);
-		fileContents = { 'users': [] };
+		fileContents = {'users': []};
 	}
 	return fileContents;
 }
 
-function updateFile(contents){
-	if (contents != null && typeof contents != 'undefined' && typeof contents != 'null' && contents != 'null'){
+function updateFile(contents) {
+	if (contents != null && typeof contents != 'undefined' && typeof contents != 'null' && contents != 'null') {
 		logger('info', 'updateFile', contents);
 		fs.truncateSync('usr.json');
 		fs.writeFileSync('usr.json', contents, {'encoding': 'utf8'});
@@ -35,31 +37,34 @@ function updateFile(contents){
 	}
 }
 
-function newUser(user){
+function newUser(user) {
 	var usrindex = -1;
 	var fileContents = fileChecks();
-	fileContents.users.forEach(function(filevalue, fileindex){
-		if (user.username == filevalue.username){
+	fileContents.users.forEach(function (filevalue, fileindex) {
+		if (user.username == filevalue.username) {
 			usrindex = fileindex;
 		}
 	});
-	if (usrindex == -1){
-		fileContents.users.push({ 'username': user.username, 'coins': 0 });
+
+	if (usrindex == -1) {
+		fileContents.users.push({'username': user.username, 'coins': 0});
 		return fileContents;
 	} else {
 		return null;
 	}
 }
 
-function addCoin(user, amount){
-	var fileContents = fileChecks();
-	var usrindex = -1;
-	fileContents.users.forEach(function(value,index){
-		if (value.username == user){
+function addCoin(user, amount) {
+	var fileContents = fileChecks(),
+		usrindex = -1;
+
+	fileContents.users.forEach(function (value, index) {
+		if (value.username == user) {
 			usrindex = index;
 		}
 	});
-	if (usrindex != -1){
+
+	if (usrindex != -1) {
 		fileContents.users[usrindex].coins += amount;
 		return fileContents;
 	} else {
@@ -68,16 +73,18 @@ function addCoin(user, amount){
 	}
 }
 
-function getCoin(user){
-	var fileContents = fileChecks();
-	var coins = -1;
-	fileContents.users.forEach(function(value,index){
-		if (value.username == user){
+function getCoin(user) {
+	var fileContents = fileChecks(),
+		coins = -1;
+
+	fileContents.users.forEach(function (value, index) {
+		if (value.username == user) {
 			coins = value.coins;
 		}
 	});
-	if (coins != -1){
-		logger('info', 'getCoin', user + ' has ' + coins + ' coins.');
+
+	if (coins != -1) {
+		logger('info', 'getCoin', user + ' has ' + coins + ' credits.');
 		return coins;
 	} else {
 		logger('info', 'getCoin', user + ' is not found.');
@@ -85,74 +92,91 @@ function getCoin(user){
 	}
 }
 
-bot.on('roomJoin', function(room) {
+function connect() {
+	bot.connect(creds.room);
+}
+
+bot.on('roomJoin', function (room) {
 	logger('info', 'Joined', 'Joined' + room);
-	bot.sendChat('Successfully started PLUR Bot: '+bot.getSelf().username+'. Type !help for a list of commands');
+	bot.sendChat('Successfully started PLUR Bot: ' + bot.getSelf().username + '. Type !help for a list of commands');
 	var djs = 0;
 
-	bot.getWaitList().forEach(function(){
+	bot.getWaitList().forEach(function () {
 		djs++;
 	});
 
 	logger('info', 'DJs', djs);
 
-	if (djs == 0 && bot.getDJ() == null){
-		bot.activatePlaylist(list);
+	if (djs == 0 && bot.getDJ() == null) {
+		bot.activatePlaylist(creds.playlist);
 		bot.joinBooth();
 	} else {
-		if (bot.getDJ().id != bot.getSelf().id){
+		if (bot.getDJ().id != bot.getSelf().id) {
 			bot.leaveBooth();
 		}
 	}
-	bot.getUsers().forEach(function(value, index){
+	bot.getUsers().forEach(function (value, index) {
 		updateFile(JSON.stringify(newUser(value)));
 	});
 });
 
-bot.on('userJoin', function(data){
+bot.on('userJoin', function (data) {
 	updateFile(JSON.stringify(newUser(data)));
 });
 
-bot.on('command:help', function(data){
-	bot.sendChat('Hey, @' + data.from.username + '! I am ' + bot.getSelf().username + ', auto-moderator in this room! I keep a credits system for DJs, which is kind of like a DJ rank for this room specifically. You can see your credit balance by typing !credits.');
-	bot.sendChat('I will auto skip a song if it reaches 5 Mehs. I also play music when no one else is playing.');
+bot.on('command:help', function (data) {
+	bot.sendChat('Hey, @' + data.from.username + '! I am ' + bot.getSelf().username + ', auto-moderator in this room!');
+	bot.sendChat('----- Commands -----');
+	bot.sendChat('!help : Shows this help menu.');
+	bot.sendChat('!credits : Show your DJ credit balance.');
+	// bot.sendChat('!skip : Skips your song if you\'re the DJ.');
+	bot.sendChat('----- Other Rules and Info -----');
+	bot.sendChat('I will auto skip a song if it reaches 5 Mehs.');
+	bot.sendChat('I will DJ when no one else is in queue.');
 });
 
-bot.on('command:credits', function(data){
+bot.on('command:credits', function (data) {
 	bot.sendChat('@' + data.from.username + ' You have ' + getCoin(data.from.username) + ' credits.');
 });
 
-bot.on('advance', function(data){
+// bot.on('command:skip', function () {
+// 	bot.selfSkip();
+// });
+
+bot.on('advance', function (data) {
 	var djs = 0;
-	bot.getWaitList().forEach(function(){
+	bot.getWaitList().forEach(function () {
 		djs++;
 	});
-	if (djs == 0 && bot.getDJ() == null){
-		bot.activatePlaylist(list);
+	if (djs == 0 && bot.getDJ() == null) {
+		bot.activatePlaylist(creds.playlist);
 		bot.joinBooth();
 	} else {
-		if (bot.getDJ().id != bot.getSelf().id){
+		if (bot.getDJ().id != bot.getSelf().id) {
 			bot.leaveBooth();
 		}
 		bot.woot();
 	}
-	if (bot.getDJ() != null){
+	if (bot.getDJ() != null) {
 		var dj = bot.getDJ();
 		updateFile(JSON.stringify(addCoin(dj.username, coinPerPlay)));
 	}
 });
 
-bot.on('chat', function(data){
-	if (data.message.indexOf('@' + bot.getSelf().username) > -1){
+bot.on('chat', function (data) {
+	if (data.message.indexOf('@' + bot.getSelf().username) > -1) {
 		bot.sendChat('Hey @' + data.from.username + '! I am a robot. Learn more by typing \'!help\'');
 	}
 });
 
-bot.on('vote', function(data){
-	if(bot.getRoomScore.negative >= 5){
+bot.on('vote', function (data) {
+	if (bot.getRoomScore.negative >= 5) {
 		bot.sendChat('5 Mehs reached, skipping song...');
 		bot.moderateForceSkip();
 	}
 });
 
-bot.connect(creds.room);
+bot.on('close', connect);
+bot.on('error', connect);
+
+connect();
